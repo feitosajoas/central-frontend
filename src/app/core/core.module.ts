2991f0ffbfdd8172b19317352b7c790ad70db449
Joás Feitosa <joasfeitosadelimasilva@gmail.com>
2022-12-09 15:42:47 -0300

    trabalhando em módulo core

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+
+@NgModule({
+  declarations: [HeaderComponent],
+  imports: [],
+  providers: [],
+  bootstrap: [],
+})
+export class CoreModule {}
---
+++ src/app/core/header/header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+})
+export class HeaderComponent {
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  constructor() {}
+}
